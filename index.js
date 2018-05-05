200f5e0f670bd5238da945813fd3531b770e9657
Nikhil Arora <narora200@gmail.com>
2018-05-05 20:19:41 +0530

    routes setup

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const app = express();
 app.listen(3333, function(){
 	console.log('Listening to port 3333');
 })
+
+
+app.get('/', function(req, res) {
+  res.send('Hello Universe')
+})
+
+app.get('/test', function(req, res) {
+  res.send('Test successfull!')
+})
