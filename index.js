75c3c5b88cee34d986fd530f69efab619b0b907c
Nikhil Arora <nikhilarora@maplegraph.com>
2018-05-06 17:38:43 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-
+let count = 2;
+let todos = [{
+  id: '0',
+  content: 'Buy milk',
+  isCompleted: true
+},
+{
+  id: '1',
+  content: 'Cook some lobster',
+  isCompleted: false
+}];
 
 const resolvers = {
   Query: {
-    whatsForDinner: (_, args) => {
-      const idx = Math.floor(Math.random() * dinnerOptions.length);
-      const foodChoice = dinnerOptions[idx];
-      return `Tonight we eat ${foodChoice}`;
+    allTodos: () => {
+      return todos;
+    },
+    Todo: (_, { id }) => {
+      const todo = todos.find(x => x.id === id);
+      if (!todo) {
+        throw new Error('Cannot find your todo!');
+      }
+      return todo;
     }
   },
+  Mutation: {
+    createTodo: (_, { content, isCompleted }) => {
+      const newTodo = {
+        id: count++,
+        content,
+        isCompleted
+      }
+      todos = [...todos, newTodo];
+      return newTodo;
+    },
+    updateTodo: (_, { id, content, isCompleted }) => {
+      let updatedTodo;
+
+      todos = todos.map(todo => {
+        if (todo.id === id) {
+          updatedTodo = {
+            id: todo.id,
+            // for content and isCompleted, we first check if values are provided
+            content: content !== undefined ? content : todo.content,
+            isCompleted: isCompleted !== undefined ? isCompleted : todo.isCompleted
+          }
+          return updatedTodo;
+        } else {
+          return todo
+        }
+      });
+
+      return updatedTodo;
+    },
+    deleteTodo: (_, { id }) => {
+      const todoToDelete = todos.find(x => x.id === id);
+
+      todos = todos.filter(todo => {
+        return todo.id !== todoToDelete.id;
+      });
+
+      return todoToDelete;
+    }
+  }
 }
 
 const options = {
