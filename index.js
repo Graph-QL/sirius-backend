b80084bc86136e678e6af3ffe5991ef608ec348c
Nikhil Arora <narora200@gmail.com>
2018-05-06 10:28:08 +0530

    going in the right direction

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql')
+
 const app = express();
 
 app.listen(3333, function(){
 	console.log('Listening to port 3333');
 })
 
+app.use('/graphql', graphqlHTTP({
+  graphiql: true
+}))
 
 app.get('/', function(req, res) {
   res.send('Hello Universe')
